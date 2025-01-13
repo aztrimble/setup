78441635295b59628cb402d208625de3d5413556
A Trimble <atrimble@hawaii.edu>
2025-01-12 21:28:46 -1000

    Add system configuration scripts: specifically swapcaps

---
+++ SetupScripts/3.0.configure_all.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+
+# Script file to run all configuration scripts (and thus apply all configurations).
+#
+# Revision History
+# 2025.01.12, A Trimble (atrimble@hawaii.edu)
+#   - Initial creation
+
+## Run the system configuration script 
+. ~/SetupFiles/SetupScripts/3.1.configure_system.sh
---
+++ SetupScripts/3.1.configure_system.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+
+# Script file to make system level configuation changes. Things like keyboard bindings.
+#
+# Revision History
+# 2025.01.12, A Trimble (atrimble@hawaii.edu)
+#   - Initial creation
+#   - ToDo
+#       - Absolute and known file paths are assumed.
+
+## Swap "Caps Lock" and "Ctrl" keys
+# First create a backup of the original keyboard file if it doesn't exist
+if [ -f "/etc/default/keyboard.original" ]; then
+    echo "Backup version of keyboard file already exists"
+else
+    echo "Creating backup of keyboard file (keyboard.original)"
+    sudo cp /etc/default/keyboard /etc/default/keyboard.original
+    echo "Appending"
+    cat ~/SetupFiles/ConfigFiles/custom_settings_tag | sudo tee -a /etc/default/keyboard
+    echo "to the bottom of the keyboard file"
+fi
+# Next append the contents of the keyboard_swapcaps file to the system keyboard file
+if grep -q 'XKBOPTIONS="ctrl:swapcaps"' '/etc/default/keyboard'; then
+    echo "The keyboard file already contains the swapcaps configuration"
+else
+    echo "Appending"
+    cat ~/SetupFiles/ConfigFiles/swapcaps | sudo tee -a /etc/default/keyboard
+    echo "to the bottom of the keyboard file"
+fi
